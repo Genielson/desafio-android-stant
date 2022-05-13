eaf4ca4dadf8045afd257e8a9662140cfef83e2e
Genielson <ge.nielson@hotmail.com>
2022-05-13 20:27:04 -0300

    Feature/Implementando Splash Screen com Logo

--- app/src/main/java/com/example/desafio_android_stant/SplashScreenActivity.kt
+++ app/src/main/java/com/example/desafio_android_stant/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.example.desafio_android_stant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        callSplashScreenWithTime()
+    }
+
+    private fun callSplashScreenWithTime(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },3000)
     }
 }
